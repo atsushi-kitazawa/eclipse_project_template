e674ae5665314cbbc67508a36cdeab10b0d3404d
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-04-20 22:47:38 +0900

    create eclipse java project, maven project template.

---
+++ java_project/copy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp /Users/atsushi/workspace/eclipse_project_template/java_project/.gitignore .
+cp /Users/atsushi/workspace/eclipse_project_template/java_project/.classpath .
+cp /Users/atsushi/workspace/eclipse_project_template/java_project/.project .
+cp -r /Users/atsushi/workspace/eclipse_project_template/java_project/.settings .
+mkdir src
---
+++ maven_project/copy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp /Users/atsushi/workspace/eclipse_project_template/maven_project/.classpath .
+cp /Users/atsushi/workspace/eclipse_project_template/maven_project/.project .
+cp /Users/atsushi/workspace/eclipse_project_template/maven_project/pom.xml .
+cp -r /Users/atsushi/workspace/eclipse_project_template/maven_project/.settings .
+cp -r /Users/atsushi/workspace/eclipse_project_template/maven_project/src .
+cp -r /Users/atsushi/workspace/eclipse_project_template/maven_project/target .
